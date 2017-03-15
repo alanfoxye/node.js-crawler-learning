4fb3f9484b89c7903e7a0d6fd8925f97cc2ea91f
alanfoxye <Alanye@msn.com>
2017-03-15 10:19:19 +0800

    Update first_crawler.js

--- first_crawler.js
+++ first_crawler.js
@@ -1,8 +1,8 @@
-/* 2017.3.15 Node.JS Crawler by alanfoxye*/
+/* 2017.3.15 Node.JS crawler by alanfoxye*/
 
 var myRequest = require('request')
 var myCheerio = require('cheerio')
-var myURL = 'http://e.icis-china.com/news/detail?id=1161&typeid=15'
+var myURL = 'http://e.icis-china.com/news/detail?id=1160&typeid=15'
 
 //request module fetching url
 function request(url, callback) {
@@ -18,14 +18,6 @@ function request(url, callback) {
 
 request(myURL, function (err, res, body) {
     var html = body;
-    //console.log(html);
-    var $ = myCheerio.load(html, { decodeEntities: true });
-    console.log("encoding: "+$('meta').attr("charset"));
-    console.log("keywords: " + $('meta[name="keywords"]').eq(1).attr("content"));
-    console.log("title: " + $('title').text());
-    console.log("date: " + $('span[class="date"]').eq(0).text());
-    console.log("browse: " + $('span[class="date"]').eq(1).text());
-    console.log("author: " + $('.editor>p').eq(0).text());
-    console.log("content: " + $('.editor').text());
-    //console.log($('.editor').html())
+    var $ = myCheerio.load(html, { decodeEntities: false });
+    console.log($.html());    
 })
